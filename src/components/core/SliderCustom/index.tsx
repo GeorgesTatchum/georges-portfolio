ee4d3c3b3fed7396c9aead649681447e94b6305e
GeorgesTatchum <153494397+GeorgesTatchum@users.noreply.github.com>
2024-08-02 09:53:18 +0200

    update and add some animation

--- src/components/core/SliderCustom/index.tsx
+++ src/components/core/SliderCustom/index.tsx
@@ -33,11 +33,11 @@ const SliderCustom = (props: { items: ProjectType[], autoSlide: boolean, }) => {
     return (
         <section className="min-h-[calc(100vh-80px)] flex flex-col items-center justify-center relative overflow-hidden w-full ">
             <div className="absolute top-1/2 flex justify-between w-full z-10 px-4 xs:hidden">
-                <button onClick={handlePrev} className="bg-gray-800 rounded-l-md">
-                    <ArrowLeftSquareFill size={42} />
+                <button onClick={handlePrev} className={`bg-gray-800 rounded-l-md`}>
+                    <ArrowLeftSquareFill size={42} className={`${styles.arrow_left}`} />
                 </button>
-                <button onClick={handleNext} className="bg-gray-800 rounded-r-md">
-                    <ArrowRightSquareFill size={42} />
+                <button onClick={handleNext} className={`bg-gray-800 rounded-r-md`}>
+                    <ArrowRightSquareFill size={42} className={`${styles.arrow_right}`}/>
                 </button>
             </div>
             <div
